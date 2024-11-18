c05164f0379a70cd5434c7da936ebf75ee91ba68
OnurBaha <koconurbaha@gmail.com>
2024-11-18 22:30:27 +0300

    added products and modal-popup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './machine_test/products/products.component';
+import { ModalPopupComponent } from './pages/modal-popup/modal-popup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'modal-popup',
+        component:ModalPopupComponent
+    },
+    {
+        path:'products',
+        component:ProductsComponent
+    }
+
+];
